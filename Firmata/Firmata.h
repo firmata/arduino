753e99ed7d0f02356099b68b9f30f8cef4c6dbd7
eighthave <eighthave@00ef7af9-2ed6-4181-8b7a-87b5c9b77d3d>
2011-01-03 04:18:27 +0000

    added bugfix version number

--- Firmata/Firmata.h
+++ Firmata/Firmata.h
@@ -23,6 +23,7 @@
  * installed firmware. */
 #define FIRMATA_MAJOR_VERSION   2 // for non-compatible changes
 #define FIRMATA_MINOR_VERSION   2 // for backwards compatible changes
+#define FIRMATA_BUGFIX_VERSION  1 // for bugfix releases
 
 #define MAX_DATA_BYTES 32 // max number of data bytes in non-Sysex messages
 
