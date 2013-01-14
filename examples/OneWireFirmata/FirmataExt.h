d64c837acecd4b2e2326799726d71508c9f503b3
ntruchsess <norbert.truchsess@t-online.de>
2013-01-15 00:58:52 +0100

    refactoring: OneWire - message format to optimize both flexibility and size. Generic scheduler: schedule series of firmata-messages, allow building tasks bigger than MAX_MESSAGE_SIZE

--- examples/OneWireFirmata/FirmataExt.h
+++ examples/OneWireFirmata/FirmataExt.h
@@ -1,25 +1,6 @@
 #ifndef FirmataExt_h
 #define FirmataExt_h
-
 #include "Arduino.h"
-#include "Firmata.h"
 
-class FirmataExtClass
-{
-  public:
-  void attach(sysexCallbackFunction newFunction);
-
-  void sendBinarySysex(byte command, byte numBytes, byte *data);
-  void sysexCallback(byte command, byte argc, byte *argv);
-
-  private:
-  byte decodedBytes[((MAX_DATA_BYTES-1)*7)/8];
-  sysexCallbackFunction decodedSysexCallback;
-
-  byte readBinarySysex(byte numInBytes, byte *inData);
-};
-
-extern FirmataExtClass FirmataExt;
-
+#define FirmataWrite(a)(Serial.write((a)))
 #endif
-
