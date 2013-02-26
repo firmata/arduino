0b3581500cdfc17ff7dc0a200e31a9727089a7dd
ntruchsess <norbert.truchsess@t-online.de>
2013-02-26 11:25:34 +0100

    add all known contributors to copyright notice

--- examples/StandardFirmata/AnalogWrite.h
+++ examples/StandardFirmata/AnalogWrite.h
@@ -1,6 +1,9 @@
 /*
   AnalogWrite.h - Firmata library
   Copyright (C) 2006-2008 Hans-Christoph Steiner.  All rights reserved.
+  Copyright (C) 2010-2011 Paul Stoffregen.  All rights reserved.
+  Copyright (C) 2009 Shigeru Kobayashi.  All rights reserved.
+  Copyright (C) 2009-2011 Jeff Hoefs.  All rights reserved.
   Copyright (C) 2013 Norbert Truchsess. All rights reserved.
 
   This library is free software; you can redistribute it and/or
