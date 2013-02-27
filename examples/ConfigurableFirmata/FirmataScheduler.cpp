790e56b7270b05f1d5d4efcdea3d1da6ea2a9bcf
ntruchsess <norbert.truchsess@t-online.de>
2013-02-27 01:45:54 +0100

    update copyright notice for OneWireFirmata and FirmataScheduler

--- examples/ConfigurableFirmata/FirmataScheduler.cpp
+++ examples/ConfigurableFirmata/FirmataScheduler.cpp
@@ -1,3 +1,15 @@
+/*
+  FirmataScheduler.cpp - Firmata library
+  Copyright (C) 2012-2013 Norbert Truchsess. All rights reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  See file LICENSE.txt for further informations on licensing terms.
+*/
+
 #include "Arduino.h"
 #include "Encoder7Bit.h"
 #include "FirmataScheduler.h"
