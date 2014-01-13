967030ec6c12d72e650c1818c472e01e98281a91
Jeff Hoefs <jeff.hoefs@gmail.com>
2014-01-12 21:37:37 -0500

    added utility directory to release zip and set correct version number on zip file (configurable is v2.5.0)

--- release.sh
+++ release.sh
@@ -7,6 +7,7 @@
 mkdir -p temp/Firmata
 cp -r examples temp/Firmata
 cp -r extras temp/Firmata
+cp -r utility temp/Firmata
 cp Boards.h temp/Firmata
 cp Firmata.cpp temp/Firmata
 cp Firmata.h temp/Firmata
@@ -16,7 +17,7 @@ cd temp
 find . -name "*.DS_Store" -type f -delete
 zip -r Firmata.zip ./Firmata/
 cd ..
-mv ./temp/Firmata.zip Firmata-2.4.0.zip
+mv ./temp/Firmata.zip Firmata-2.5.0.zip
 
 #package for Arduino 1.5.x
 cp library.properties temp/Firmata
@@ -26,9 +27,10 @@ mkdir src
 mv Boards.h ./src/
 mv Firmata.cpp ./src/
 mv Firmata.h ./src/
+mv utility ./src/
 cd ..
 find . -name "*.DS_Store" -type f -delete
 zip -r Firmata.zip ./Firmata/
 cd ..
-mv ./temp/Firmata.zip Arduino-1.5.x-Firmata-2.4.0.zip
+mv ./temp/Firmata.zip Arduino-1.5.x-Firmata-2.5.0.zip
 rm -r ./temp
