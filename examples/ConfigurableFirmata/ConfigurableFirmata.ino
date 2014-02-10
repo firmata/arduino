3a6aacf4cdcddbd06f9630468b68de45719dac08
Nicolas Panel <nicolas.panel@live.fr>
2014-02-10 12:28:56 -0500

    update ConfigurableFirmata to manage Encoders

--- examples/ConfigurableFirmata/ConfigurableFirmata.ino
+++ examples/ConfigurableFirmata/ConfigurableFirmata.ino
@@ -105,13 +105,16 @@ FirmataExt firmataExt;
 #include <utility/FirmataScheduler.h>
 FirmataScheduler scheduler;
 
+#include <utility/EncoderFirmata.h>
+EncoderFirmata encoder;
+
 
 // dependencies. Do not comment out the following lines
 #if defined AnalogOutputFirmata_h || defined ServoFirmata_h
 #include <utility/AnalogWrite.h>
 #endif
 
-#if defined AnalogInputFirmata_h || defined I2CFirmata_h
+#if defined AnalogInputFirmata_h || defined I2CFirmata_h || defined EncoderFirmata_h
 #include <utility/FirmataReporting.h>
 FirmataReporting reporting;
 #endif
@@ -220,6 +223,9 @@ void setup()
 #ifdef FirmataScheduler_h
   firmataExt.addFeature(scheduler);
 #endif
+#ifdef EncoderFirmata_h
+  firmataExt.addFeature(encoder);
+#endif
 #endif
   /* systemResetCallback is declared here (in ConfigurableFirmata.ino) */
   Firmata.attach(SYSTEM_RESET, systemResetCallback);
@@ -289,6 +295,10 @@ runtasks: scheduler.runTasks();
 #ifdef I2CFirmata_h
     // report i2c data for all device with read continuous mode enabled
     i2c.report();
+#endif
+#ifdef EncoderFirmata_h
+    // report encoders positions if reporting enabled.
+    encoder.report();
 #endif
   }
 #endif
