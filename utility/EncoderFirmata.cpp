ccfe8affe28d41ca0968b76f688928544beff9ba
Nicolas Panel <nicolas.panel@live.fr>
2014-02-10 08:45:10 -0500

    add EncoderFirmata implementation file

---
+++ utility/EncoderFirmata.cpp
@@ -0,0 +1,24 @@
+/*
+  EncoderFirmata.cpp - Firmata library
+  Copyright (C) 2006-2008 Hans-Christoph Steiner.  All rights reserved.
+  Copyright (C) 2010-2011 Paul Stoffregen.  All rights reserved.
+  Copyright (C) 2009 Shigeru Kobayashi.  All rights reserved.
+  Copyright (C) 2009-2011 Jeff Hoefs.  All rights reserved.
+  Copyright (C) 2013 Norbert Truchsess. All rights reserved.
+  Copyright (C) 2013 Nicolas Panel. All rights reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  See file LICENSE.txt for further informations on licensing terms.
+  
+  Provide encoder implementation based on Arduino external interrupts
+  see http://arduino.cc/en/Reference/attachInterrupt for more informations
+*/
+
+#include <Firmata.h>
+#include <EncoderFirmata.h>
+
+
