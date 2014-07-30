0893eb582bf80a281da35e2dd685820883061b23
Jacob Rosenthal <jakerosenthal@gmail.com>
2014-07-29 20:16:30 -0700

    Spark support

--- Firmata.cpp
+++ Firmata.cpp
@@ -15,7 +15,10 @@
 //******************************************************************************
 
 #include "Firmata.h"
+
+#ifndef SPARK
 #include "HardwareSerial.h"
+#endif
 
 extern "C" {
 #include <string.h>
@@ -28,8 +31,8 @@ extern "C" {
 
 void FirmataClass::sendValueAsTwo7bitBytes(int value)
 {
-  FirmataSerial->write(value & B01111111); // LSB
-  FirmataSerial->write(value >> 7 & B01111111); // MSB
+  FirmataSerial->write(value & 127); // LSB
+  FirmataSerial->write(value >> 7 & 127); // MSB
 }
 
 void FirmataClass::startSysex(void)
