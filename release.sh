8a92437733864d7f6e7973dd9602e62fdd974802
Jeff Hoefs <jeff.hoefs@gmail.com>
2014-08-03 22:13:55 -0700

    updated release script to not create src directory for Arduino 1.5.x. Now create single zip for all Arduino 1.0.x and 1.5.x

--- release.sh
+++ release.sh
@@ -3,8 +3,9 @@
 # use this script to package Firmata for distribution
 # to do: make an ant script or something else that is cross platform
 
-# package for Arduino 1.0.x
+# create distribution package for Arduino 1.0.x and Arduino 1.5.x
 mkdir -p temp/Firmata
+cp library.properties temp/Firmata
 cp -r examples temp/Firmata
 cp -r extras temp/Firmata
 cp Boards.h temp/Firmata
@@ -17,18 +18,4 @@ find . -name "*.DS_Store" -type f -delete
 zip -r Firmata.zip ./Firmata/
 cd ..
 mv ./temp/Firmata.zip Firmata-2.4.0-beta1.zip
-
-#package for Arduino 1.5.x
-cp library.properties temp/Firmata
-cd temp/Firmata
-mv readme.md ./extras/
-mkdir src
-mv Boards.h ./src/
-mv Firmata.cpp ./src/
-mv Firmata.h ./src/
-cd ..
-find . -name "*.DS_Store" -type f -delete
-zip -r Firmata.zip ./Firmata/
-cd ..
-mv ./temp/Firmata.zip Arduino-1.5.x-Firmata-2.4.0-beta1.zip
 rm -r ./temp
