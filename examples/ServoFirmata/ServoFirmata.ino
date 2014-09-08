11fa28d05c72714117a74b02e9c795a481216a82
Jeff Hoefs <jeff.hoefs@gmail.com>
2014-09-07 21:04:27 -0700

    updated and tested ServoFirmata example

--- examples/ServoFirmata/ServoFirmata.ino
+++ examples/ServoFirmata/ServoFirmata.ino
@@ -29,28 +29,33 @@ void analogWriteCallback(byte pin, int value)
   }
 }
 
+void systemResetCallback()
+{
+  servoCount = 0;
+}
+
 void setup() 
 {
   byte pin;
 
   Firmata.setFirmwareVersion(FIRMATA_MAJOR_VERSION, FIRMATA_MINOR_VERSION);
   Firmata.attach(ANALOG_MESSAGE, analogWriteCallback);
+  Firmata.attach(SYSTEM_RESET, systemResetCallback);
+  
+  Firmata.begin(57600);
+  systemResetCallback();
 
-  // attach servos from fist digital pin up to max number of
+  // attach servos from first digital pin up to max number of
   // servos supported for the board
   for (pin = 0; pin < TOTAL_PINS; pin++) {
     if (IS_PIN_DIGITAL(pin)) {
       if (servoCount < MAX_SERVOS) {
-        servos[servoPinMap[pin]].attach(PIN_TO_DIGITAL(pin));
         servoPinMap[pin] = servoCount;
+        servos[servoPinMap[pin]].attach(PIN_TO_DIGITAL(pin));
         servoCount++;
-      } else {
-        Firmata.sendString("Max servos attached");
       }
     }
   }
-  
-  Firmata.begin(57600);
 }
 
 void loop() 
