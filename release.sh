03de47f0680e5445f382e0666692ff806f844405
Jeff Hoefs <jeff.hoefs@gmail.com>
2014-11-01 21:19:08 -0700

    revert release.sh to create separate archives for arduino 1.x and 1.5.x

--- release.sh
+++ release.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
 # use this script to package Firmata for distribution
-# to do: make an ant script or something else that is cross platform
 
-# create distribution package for Arduino 1.0.x and Arduino 1.5.x
+# package for Arduino 1.0.x
 mkdir -p temp/Firmata
-cp library.properties temp/Firmata
 cp -r examples temp/Firmata
 cp -r extras temp/Firmata
 cp Boards.h temp/Firmata
@@ -17,5 +15,19 @@ cd temp
 find . -name "*.DS_Store" -type f -delete
 zip -r Firmata.zip ./Firmata/
 cd ..
-mv ./temp/Firmata.zip Firmata-2.4.0-beta2.zip
+mv ./temp/Firmata.zip Firmata-2.4.0-beta3.zip
+
+#package for Arduino 1.5.x
+cp library.properties temp/Firmata
+cd temp/Firmata
+mv readme.md ./extras/
+mkdir src
+mv Boards.h ./src/
+mv Firmata.cpp ./src/
+mv Firmata.h ./src/
+cd ..
+find . -name "*.DS_Store" -type f -delete
+zip -r Firmata.zip ./Firmata/
+cd ..
+mv ./temp/Firmata.zip Arduino-1.5.x-Firmata-2.4.0-beta3.zip
 rm -r ./temp
