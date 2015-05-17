14489cc1034033bc6b638858d2c83164e2d5d52b
Jeff Hoefs <jeff.hoefs@gmail.com>
2015-05-16 21:24:25 -0700

    added copyright header to boards.h

--- Boards.h
+++ Boards.h
@@ -1,4 +1,14 @@
-/* Boards.h - Hardware Abstraction Layer for Firmata library */
+/*
+  Boards.h - Hardware Abstraction Layer for Firmata library
+  Copyright (c) 2006-2008 Hans-Christoph Steiner.  All rights reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  See file LICENSE.txt for further informations on licensing terms.
+*/
 
 #ifndef Firmata_Boards_h
 #define Firmata_Boards_h
--- Firmata.h
+++ Firmata.h
@@ -1,6 +1,6 @@
 /*
   Firmata.h - Firmata library v2.4.3 - 2015-4-11
-  Copyright (C) 2006-2008 Hans-Christoph Steiner.  All rights reserved.
+  Copyright (c) 2006-2008 Hans-Christoph Steiner.  All rights reserved.
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
