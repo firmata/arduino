74b036386f0969529732b82c3b3bcf2545a816ba
Christopher Stawarz <cstawarz@mit.edu>
2021-11-01 09:43:44 -0400

    Clarify why the implementations of ArduinoBLE_UART_Stream and BluefruitLE_SPI_Stream are in their respective header files

--- utility/ArduinoBLE_UART_Stream.cpp
+++ utility/ArduinoBLE_UART_Stream.cpp
@@ -1,3 +1,4 @@
 /*
- * Implementation is in ArduinoBLE_UART_Stream.h to avoid linker issues.
+ * Implementation is in ArduinoBLE_UART_Stream.h to avoid making ArduinoBLE a
+ * build-time dependency for all projects that use the Firmata library.
  */
