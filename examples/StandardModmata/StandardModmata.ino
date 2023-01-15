6605a8687c9bc243f957303923494101547b2402
Sam Hutcherson <shutche@siue.edu>
2023-01-15 17:40:03 -0600

    Added example arduino sketch

---
+++ examples/StandardModmata/StandardModmata.ino
@@ -0,0 +1,11 @@
+#include <Modmata.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Modmata.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Modmata.update();
+}
